8267ba7ba8e3a9458b07c1341b1ac37d4b4ba689
h2oos <h2oos@proton.me>
2024-08-10 00:08:46 -0400

    message added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 'use client'
-import Image from "next/image";
-import { useState } from "react";
+import Image from 'next/image'
+import { useState} from 'react'
+import { BOX } from '@mui/material'
 
 export default function Home() {
   const [messages, setMessages] = useState({
     role: 'chatbot',
-    content: `Hi! I'm a basketball chatbot. Ask me questions you have about teams, players, games, stats, and other NBA-related topics`
+    content: `Hi! I'm a basketball chatbot. Ask me questions you have about teams, players, games, stats, and other NBA-related topics`,
   })
+
+  const [message, setMessage] = useState('')
+  
+  return(<BOX></BOX>)
 }
