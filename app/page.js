af6451b1c1868a2c03bc031b1f2bd9cc4ab9bec9
h2oos <h2oos@proton.me>
2024-08-10 08:32:24 -0400

    imported stack

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
 import { useState} from 'react'
-import { Box } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 
 export default function Home(){
   const [messages, setMessages] = useState({
