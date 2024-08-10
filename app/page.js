994a43f25126fd38213fad89ec7c035d7dce49ce
h2oos <h2oos@proton.me>
2024-08-09 23:53:20 -0400

    added chatbot message

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 'use client'
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  
+  const [messages, setMessages] = useState({
+    role: 'chatbot',
+    content: `Hi! I'm a basketball chatbot. Ask me questions you have about teams, players, games, stats, and other NBA-related topics`
+  })
 }
