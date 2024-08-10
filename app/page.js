ff6b0b98c720ce77ddadb73ddcacaed281321da9
h2oos <h2oos@proton.me>
2024-08-10 00:21:25 -0400

    fixed runtime error

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 'use client'
 import Image from 'next/image'
 import { useState} from 'react'
-import { BOX } from '@mui/material'
+import { Box } from '@mui/material'
 
-export default function Home() {
+export default function Home(){
   const [messages, setMessages] = useState({
-    role: 'chatbot',
-    content: `Hi! I'm a basketball chatbot. Ask me questions you have about teams, players, games, stats, and other NBA-related topics`,
+    role: 'assistant',
+    content: `Hi! I'm a basketball chatbot. Ask me questions you have about teams, players, games, stats, and other NBA-related topics` , 
   })
 
   const [message, setMessage] = useState('')
   
-  return(<BOX></BOX>)
-}
+  return(<Box></Box>)
+} 
